001fdb762e661cb938ff4744536f21bd05f9bbb4
wildbook7 <wildbook7@gmail.com>
2015-10-04 23:00:06 +0900

    Create index.js

---
+++ public/javascripts/index.js
@@ -0,0 +1,18 @@
+function move() {
+  // ボタンIDを基準に移動
+}
+
+/**
+ * 移動ボタンのdisabled設定
+ */
+function setBottons() {
+  $("#dishA").disabled(false);
+}
+
+function start() {
+  // dishAに設定し、ゲーム開始
+  $("#dishA").val("123");
+  $("#dishB").val("");
+  $("#dishC").val("");
+  setBottons();
+}
