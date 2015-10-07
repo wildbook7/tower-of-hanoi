d5bacc6e8efc797c17855fd7243e87718333ada0
wildbook7 <wildbook7@gmail.com>
2015-10-08 00:03:57 +0900

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,21 @@
-function move() {
+function move(btn) {
   // ボタンIDを基準に移動
+  var src = "#dish" + btn.id.substr(0,1);
+  var tgt = "#dish" + btn.id.substr(1,1);
+  $(tgt).val($(src).val().substr(0,1) + $(tgt).val());
+  $(src).val($(src).val().substr(1));
 }
 
 /**
  * 移動ボタンのdisabled設定
  */
 function setBottons() {
-  $("#btnAB").prop("disabled", true);
+  $("#btnAB").prop("disabled", $("#dishA").val().equals(""));
+  $("#btnAC").prop("disabled", $("#dishA").val().equals(""));
+  $("#btnBA").prop("disabled", $("#dishB").val().equals(""));
+  $("#btnBC").prop("disabled", $("#dishB").val().equals(""));
+  $("#btnCA").prop("disabled", $("#dishC").val().equals(""));
+  $("#btnCB").prop("disabled", $("#dishC").val().equals(""));
 }
 
 function start() {
