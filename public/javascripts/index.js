592744885cc7c5d58222a92f5d4314f52813a94a
wildbook7 <wildbook7@gmail.com>
2015-10-08 00:39:26 +0900

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,21 +1,22 @@
 function move(btn) {
   // ボタンIDを基準に移動
-  var src = "#dish" + btn.id.substr(0,1);
-  var tgt = "#dish" + btn.id.substr(1,1);
+  var src = "#dish" + btn.id.substr(3,1);
+  var tgt = "#dish" + btn.id.substr(4,1);
   $(tgt).val($(src).val().substr(0,1) + $(tgt).val());
   $(src).val($(src).val().substr(1));
+  setBottons();
 }
 
 /**
  * 移動ボタンのdisabled設定
  */
 function setBottons() {
-  $("#btnAB").prop("disabled", $("#dishA").val().equals(""));
-  $("#btnAC").prop("disabled", $("#dishA").val().equals(""));
-  $("#btnBA").prop("disabled", $("#dishB").val().equals(""));
-  $("#btnBC").prop("disabled", $("#dishB").val().equals(""));
-  $("#btnCA").prop("disabled", $("#dishC").val().equals(""));
-  $("#btnCB").prop("disabled", $("#dishC").val().equals(""));
+  $("#btnAB").prop("disabled", $("#dishA").val() == "");
+  $("#btnAC").prop("disabled", $("#dishA").val() == "");
+  $("#btnBA").prop("disabled", $("#dishB").val() == "");
+  $("#btnBC").prop("disabled", $("#dishB").val() == "");
+  $("#btnCA").prop("disabled", $("#dishC").val() == "");
+  $("#btnCB").prop("disabled", $("#dishC").val() == "");
 }
 
 function start() {
