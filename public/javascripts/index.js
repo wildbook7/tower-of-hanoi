c582fca18ffd4674e46ded331f491c813acf80d0
wildbook7 <wildbook7@gmail.com>
2015-10-09 00:47:26 +0900

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,8 @@
 function move(btn) {
   // ボタンIDを基準に移動
-  var src = "#dish" + btn.id.substr(3,1);
-  var tgt = "#dish" + btn.id.substr(4,1);
-  $(tgt).val($(src).val().substr(0,1) + $(tgt).val());
+  var src = "#dish" + btn.id.substr(3, 1);
+  var tgt = "#dish" + btn.id.substr(4, 1);
+  $(tgt).val($(src).val().substr(0, 1) + $(tgt).val());
   $(src).val($(src).val().substr(1));
   setBottons();
 }
@@ -14,15 +14,15 @@ function setBottons() {
   var a = $("#dishA").val();
   var b = $("#dishB").val();
   var c = $("#dishC").val();
-  a = (a == "")? "9" : a.substr(0,1);
-  b = (b == "")? "9" : b.substr(0,1);
-  c = (c == "")? "9" : c.substr(0,1);
-  $("#btnAB").prop("disabled", (a > b));
-  $("#btnAC").prop("disabled", (a > c));
-  $("#btnBC").prop("disabled", (b > c));
-  $("#btnBA").prop("disabled", (b > a));
-  $("#btnCA").prop("disabled", (c > a));
-  $("#btnCB").prop("disabled", (c > b));
+  a = (a == "")? "9" : a.substr(0, 1);
+  b = (b == "")? "9" : b.substr(0, 1);
+  c = (c == "")? "9" : c.substr(0, 1);
+  $("#btnAB").prop("disabled", (a >= b));
+  $("#btnAC").prop("disabled", (a >= c));
+  $("#btnBC").prop("disabled", (b >= c));
+  $("#btnBA").prop("disabled", (b >= a));
+  $("#btnCA").prop("disabled", (c >= a));
+  $("#btnCB").prop("disabled", (c >= b));
 }
 
 function start() {
