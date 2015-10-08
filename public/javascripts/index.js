64080c73a3a48ce79a57ca3de6eafc3fdf25b7a1
wildbook7 <wildbook7@gmail.com>
2015-10-09 00:28:38 +0900

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -14,12 +14,12 @@ function setBottons() {
   var a = $("#dishA").val().substr(0,1);
   var b = $("#dishB").val().substr(0,1);
   var c = $("#dishC").val().substr(0,1);
-  $("#btnAB").prop("disabled", (a == "" || a > b);
-  $("#btnAC").prop("disabled", (a == "" || a > c);
-  $("#btnBC").prop("disabled", (b == "" || b > c);
-  $("#btnBA").prop("disabled", (b == "" || b > a);
-  $("#btnCA").prop("disabled", (c == "" || c > a);
-  $("#btnCB").prop("disabled", (c == "" || c > b);
+  $("#btnAB").prop("disabled", (a == "" || a > b));
+  $("#btnAC").prop("disabled", (a == "" || a > c));
+  $("#btnBC").prop("disabled", (b == "" || b > c));
+  $("#btnBA").prop("disabled", (b == "" || b > a));
+  $("#btnCA").prop("disabled", (c == "" || c > a));
+  $("#btnCB").prop("disabled", (c == "" || c > b));
 }
 
 function start() {
