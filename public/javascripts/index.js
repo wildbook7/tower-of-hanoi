06d6adbb966faef8ceb0c481254c83c8550255ec
wildbook7 <wildbook7@gmail.com>
2015-10-09 01:06:43 +0900

    Update index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -27,7 +27,7 @@ function setBottons() {
 
 function start() {
   // dishAに設定し、ゲーム開始
-  $("#dishA").val("1234");
+  $("#dishA").val("12345");
   $("#dishB").val("");
   $("#dishC").val("");
   setBottons();
